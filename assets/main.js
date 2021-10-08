cb550420f501928cea77a09c613de60ade8b1450
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-10-08 11:45:13 +0200

    dev: added selected functionnality

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,40 @@
 console.log("front end start");
+
+const selectedArticleIds = new Set();
+
+function refresh() {
+  console.log("refresh");
+  const trs = document.querySelectorAll("table tbody tr");
+  console.log("trs: ", trs);
+  for (const tr of trs) {
+    const id = tr.getAttribute("data-id");
+    if (selectedArticleIds.has(id)) {
+      tr.classList.add("selected");
+    } else {
+      tr.classList.remove("selected");
+    }
+  }
+  const suppressBtn = document.querySelector("button.suppress");
+  console.log("suppressBtn: ", suppressBtn);
+  suppressBtn.hidden = selectedArticleIds.size === 0;
+}
+
+addEventListener("DOMContentLoaded", () => {
+  refresh();
+  const trs = document.querySelectorAll("table tbody tr");
+  console.log("trs: ", trs);
+
+  for (const tr of trs) {
+    tr.addEventListener("click", function (event) {
+      console.log("click", this);
+      const id = this.getAttribute("data-id");
+      console.log("id: ", id);
+      if (selectedArticleIds.has(id)) {
+        selectedArticleIds.delete(id);
+      } else {
+        selectedArticleIds.add(id);
+      }
+      refresh();
+    });
+  }
+});
