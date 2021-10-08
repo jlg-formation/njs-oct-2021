3f692600aa8914ec60d62ef9ac38b858b7f11bae
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-10-08 15:15:32 +0200

    prod: added pm2 ecosystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [
+    {
+      script: "build/src/server.js",
+      env: {
+        GESTION_STOCK_SERVER_PORT: 3000,
+        NODE_ENV: "production",
+        GESTION_STOCK_DBURI: "mongodb://localhost:27017/gestion-stock-prod",
+      },
+      // env_production: {
+      //   PORT: 80,
+      //   NODE_ENV: "production",
+      // },
+    },
+  ],
+};
