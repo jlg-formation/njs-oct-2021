af87f724ee624291cb39144a2f87771c0a4b3130
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-10-08 10:51:13 +0200

    dev: added article ok

--- src/routers/frontend.router.ts
+++ src/routers/frontend.router.ts
@@ -1,6 +1,7 @@
 import { WebServer } from "./../WebServer";
 import express from "express";
 import { ArticleMongoService } from "../services/article-mongo.service";
+import { Article } from "../interfaces/Article";
 
 export function frontendRouter(webServer: WebServer) {
   const app = express.Router();
@@ -25,6 +26,22 @@ export function frontendRouter(webServer: WebServer) {
 
   app.get("/articles/add", (req, res) => {
     res.render("pages/add");
+  });
+
+  app.use(express.urlencoded({ extended: true }));
+  app.post("/articles/action/add", (req, res) => {
+    (async () => {
+      try {
+        const articleService = new ArticleMongoService(webServer);
+        const article = req.body as Partial<Article>;
+        console.log("article: ", article);
+        await articleService.create(article);
+        res.redirect("/articles");
+      } catch (err) {
+        console.log("err: ", err);
+        res.status(500).end();
+      }
+    })();
   });
 
   return app;
